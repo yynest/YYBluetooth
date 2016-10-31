ae4cee6407d18258ab5939addc0b32100cdd90eb
iosdev <iosdev@xfdeiMac.local>
2016-10-31 11:46:38 +0800

    Initial Commit

---
+++ YYBluetooth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYBluetooth
+//
+//  Created by iosdev on 2016/10/31.
+//  Copyright © 2016年 QSYJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYBluetooth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYBluetooth
+//
+//  Created by iosdev on 2016/10/31.
+//  Copyright © 2016年 QSYJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
